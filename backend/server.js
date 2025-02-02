94c2795b37534e9e12fbdc23bee9d0ad3d9906c3
danishdk46 <dkhan6142@gmail.com>
2025-02-03 01:47:04 +0500

    models, controllers, middlewares

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,40 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const dbConnect = require("./database/index");
+const { PORT } = require("./config/index");
+const router = require("./routes/index");
+const errorHandler = require("./middlewares/errorHandler");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// const corsOptions = {
+//   credentials: true,
+//   origin: ["http://localhost:3000"],
+// };
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const app = express();
+
+app.use(cookieParser());
+
+// app.use(cors(corsOptions));
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      return callback(null, true);
+    },
+    optionsSuccessStatus: 200,
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: "50mb" }));
+
+app.use(router);
+
+dbConnect();
+
+app.use("/storage", express.static("storage"));
+
+app.use(errorHandler);
+
+app.listen(PORT, console.log(`Backend is running on port: ${PORT}`));
